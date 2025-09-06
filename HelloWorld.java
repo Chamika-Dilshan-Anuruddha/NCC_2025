f10f235c5755979aa95bb422d64e220f0938f2ce
chamika dilshan <anuruddhaedcd@gmail.com>
2025-09-06 13:29:47 +0530

    initial commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
+
+        // Name : EDCD Anuruddha , Index NO: gscomp258
         System.out.println("Hello, World!");
     }
 }
